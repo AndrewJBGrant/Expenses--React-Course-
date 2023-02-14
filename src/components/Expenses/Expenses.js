638bc1305aaf73be8c4f8c290a739eecc472b51c
Andrew Grant <andrewgrant2009@hotmail.com>
2023-02-14 22:28:30 +0100

    small gains

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -13,10 +13,9 @@ function Expenses(props) {
     setDropDownYear(selectedYear);
   };
 
-  const filterExpenses = props.items.filter(expense => {
+  const filterExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === dropDownYear;
   });
-
 
   return (
     <div>
@@ -26,32 +25,18 @@ function Expenses(props) {
           onChangeFilter={filterChangeHandler}
         />
         {/* Dynamic!! */}
-        {filterExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
-
-        {/* !!! Non-Dynamic
-         <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        />
-
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        /> */}
+        {filterExpenses.length === 0 ? (
+          <p>No expenses for the year {dropDownYear} </p>
+        ) : (
+          filterExpenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))
+        )}
       </Card>
     </div>
   );
